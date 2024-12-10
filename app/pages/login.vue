8a9b379f27c1c625623ac6978f77bfff9339430d
Joschua <70809675+selfire1@users.noreply.github.com>
2024-12-10 18:01:44 +1000

    print out reponse message

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
+import type { FetchError } from "ofetch"
+const errorMsg = ref("")
+const responseMsg = ref("")
+
 const submitHandler = async (event: Event) => {
+  // reset variables
+  errorMsg.value = ""
+  responseMsg.value = ""
+
   const target = event.target as HTMLFormElement;
   const formData = new FormData(target);
 
-  const response = await $fetch(
-    "/api/auth/login",
-    {
-      method: "POST",
-      body: formData,
-      onResponse: (data) => {
-        console.log(data.response)
-      }
-    })
-
-  target.reset();
-};
+  try {
+    const fetchResponse = await $fetch(
+      "/api/auth/login",
+      {
+        method: "POST",
+        body: formData,
+      })
+    responseMsg.value = JSON.stringify(fetchResponse, null, 2)
+    target.reset();
+  } catch (e) {
+    errorMsg.value = (e as FetchError)?.data
+  }
+}
 </script>
 
 <template lang="pug">
-.is-container.py-2
+.is-container.py-2.space-y-10
   form(@submit.prevent="submitHandler")
     label Username
       input(type="text" name="username" autocomplete="username" required)
     label Password
       input(type="password" name="password" autocomplete="current-password" required)
     button(type="submit") Submit
+  div
+    pre response: {{ responseMsg }}
+    pre error: {{ errorMsg }}
 </template>
